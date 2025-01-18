712ac00046022b4b6dd1c40d3da3ddb1ca7d7840
bhamdi <bhamdi@student.42.fr>
2025-01-18 02:46:27 +0100

    add ex00 ex01

---
+++ 3-OOP/ex01/S1E7.py
@@ -0,0 +1,53 @@
+# S1E7.py
+
+from S1E9 import Character
+
+class Baratheon(Character):
+    """Class for Baratheon: Class representing a Baratheon family."""
+    
+    def __init__(self, first_name, is_alive=True):
+        """Constructor for Baratheon: Initialize a Baratheon family."""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def die(self):
+        """Method for Baratheon: Kill the Baratheon family."""
+        self.is_alive = False
+
+    def __str__(self):
+        """Method for Baratheon: Return a string representation of the Baratheon family."""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self):
+        """Method for Baratheon: Return a formal string representation of the Baratheon family."""
+        return self.__str__()
+
+class Lannister(Character):
+    """Class for Lannister: Class representing a Lannister family."""
+    
+    def __init__(self, first_name, is_alive=True):
+        """Constructor for Lannister: Initialize a Lannister family."""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def die(self):
+        """Method for Lannister: Kill the Lannister family."""
+        self.is_alive = False
+
+    def __str__(self):
+        """Method for Lannister: Return a string representation of the Lannister family."""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self):
+        """Method for Lannister: Return a formal string representation of the Lannister family."""
+        return self.__str__()
+
+    @classmethod
+    def create_lannister(cls, first_name, is_alive):
+        """Method for Lannister: Create a Lannister family."""
+        return cls(first_name, is_alive)
+    
---
+++ 3-OOP/ex01/S1E9.py
@@ -0,0 +1,31 @@
+# S1E9.py
+
+import sys
+from abc import ABC, abstractmethod
+
+sys.tracebacklimit = 0
+
+class Character(ABC):
+    """Class for Character: Abstract class representing a character."""
+    
+    def __init__(self, first_name, is_alive=True):
+        """Constructor for Character: Initialize a character with a first name and alive status."""
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+    @abstractmethod
+    def die(self):
+        """Method for Character: Kill the character by setting is_alive to False."""
+        pass
+
+class Stark(Character):
+    """Class for Stark: Representing a Stark character."""
+    
+    def __init__(self, first_name, is_alive=True):
+        """Constructor for Stark: Initialize a Stark character."""
+        super().__init__(first_name, is_alive)
+
+    def die(self):
+        """Method for Stark: Kill the Stark character."""
+        self.is_alive = False
+        
